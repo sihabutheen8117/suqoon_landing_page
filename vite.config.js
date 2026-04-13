5a6bd01cc0585154ec4e216a9217ce581ed5a674
Ibrahim <2005mohamedibrahim@gmail.com>
2026-04-13 11:46:53 +0530

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Suqoon.net/',
   server: {
     allowedHosts: ['.loca.lt', '.ngrok-free.dev']
   }
 })
-
-
