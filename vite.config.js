3a8f11b8e0d7f59f4978beac3f961b9b617f9c01
Mohamed Ibrahim A <mohamedibrahim.2305101@srec.ac.in>
2026-04-13 11:57:41 +0530

    chore: Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Suqoon.net/',
   plugins: [react()],
   base: '/Suqoon.net/',
   server: {
